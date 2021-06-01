cbd10198316b6b53948372e35d5962ca6036a86f
EasonWW <41312593+EasonWW@users.noreply.github.com>
2021-06-01 13:12:34 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Uniswap-V3-36541059d3414118bd1f72a7db68bc41',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Eason\' world',
+  domain: 'https://www.notion.so/Uniswap-V3-36541059d3414118bd1f72a7db68bc41',
+  author: 'Eason',
 
   // open graph metadata (optional)
   description: 'Example site description',
